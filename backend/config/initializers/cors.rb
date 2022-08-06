3d5008446116f8c900232042255ef5c3727b3ec4
NaoyaNakamori <73022482+nakamori-naoya@users.noreply.github.com>
2022-08-07 02:05:52 +0900

    chore:認証用のテーブルを追加

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -7,10 +7,11 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3021'
+    origins '*'
 
-    resource '/api/v1/*',
+    resource '*',
       headers: :any,
+      expose: %w[access-token uid client],
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
--- backend/db/migrate/20220806165233_create_active_employees.rb
+++ backend/db/migrate/20220806165233_create_active_employees.rb
@@ -4,11 +4,8 @@ class CreateActiveEmployees < ActiveRecord::Migration[6.0]
       t.string :last_name, null: false
       t.string :first_name, null: false
       t.date :birth_date, null: false
-      t.string :email, null: false, index: { unique: true }
-      t.string :password, null: false
       t.string :phone_number, null: false
       t.references :employee, null: false, foreign_key: true
-
       t.timestamps
     end
   end
