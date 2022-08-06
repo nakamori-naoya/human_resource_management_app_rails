aea804a001f2644c9867b47ed527b93775cc6c95
NaoyaNakamori <73022482+nakamori-naoya@users.noreply.github.com>
2022-08-06 21:13:01 +0900

    chore: nameカラムの追加

--- backend/db/migrate/20220806110721_create_personal_right_roles.rb
+++ backend/db/migrate/20220806110721_create_personal_right_roles.rb
@@ -2,6 +2,7 @@ class CreatePersonalRightRoles < ActiveRecord::Migration[6.0]
   def change
     create_table :personal_right_roles do |t|
       t.references :role, foreign_key: true, null: false
+      t.references :name, null: false
       t.timestamps
     end
   end
